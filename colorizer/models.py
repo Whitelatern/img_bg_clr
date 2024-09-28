d9ddeae99762f9866993a781ad8caa25e454e1ef
Whitelatern <92262520+Whitelatern@users.noreply.github.com>
2024-09-29 01:27:03 +0530

    Update models.py

--- colorizer/models.py
+++ colorizer/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
+
 class ImageUpload(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)  # Allow blank and null values
@@ -7,3 +11,12 @@ class ImageUpload(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ChatHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message_input = models.TextField()
+    bot_response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return f"{self.user.username} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
