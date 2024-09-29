e25dd363d39c6fbb73f7e88c7cef52a6abe06b23
Whitelatern <92262520+Whitelatern@users.noreply.github.com>
2024-09-29 14:58:08 +0530

    Update models.py

--- colorizer/models.py
+++ colorizer/models.py
@@ -12,11 +12,3 @@ class ImageUpload(models.Model):
     def __str__(self):
         return self.name
     
-class ChatHistory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    message_input = models.TextField()
-    bot_response = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def _str_(self):
-        return f"{self.user.username} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
